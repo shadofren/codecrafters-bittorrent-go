8fbcda9e7639da4663114f071b6140dbbbe527dc
Manh Tu <xxlaguna93@gmail.com>
2024-01-09 15:52:46 +0800

    bencode the info and get it's sha1 hash

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,12 +2,51 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha1"
 	"encoding/json"
 	"fmt"
 	"os"
 	"strconv"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
+
+func bencode(data any) ([]byte, error) {
+	encoded := make([]byte, 0)
+	var err error
+	switch d := data.(type) {
+	case []any:
+		encoded = append(encoded, 'l')
+		for _, item := range d {
+			value, _ := bencode(item)
+			encoded = append(encoded, value...)
+		}
+		encoded = append(encoded, 'e')
+	case *OrderedMap:
+		encoded = append(encoded, 'd')
+		for _, k := range d.Keys() {
+			key, _ := bencode(k)
+			encoded = append(encoded, key...)
+			v, _ := d.Get(k)
+			value, _ := bencode(v)
+			encoded = append(encoded, value...)
+		}
+		encoded = append(encoded, 'e')
+	case int:
+		encoded = append(encoded, 'i')
+		intStr := strconv.Itoa(d)
+		encoded = append(encoded, []byte(intStr)...)
+		encoded = append(encoded, 'e')
+	case string:
+		length := len(d)
+		lengthStr := strconv.Itoa(length)
+		encoded = append(encoded, []byte(lengthStr)...)
+		encoded = append(encoded, ':')
+		encoded = append(encoded, []byte(d)...)
+	default:
+		err = fmt.Errorf("not supported type")
+	}
+	return encoded, err
+}
 
 func decodeBencode(reader *bytes.Reader) (interface{}, error) {
 	c, _ := reader.ReadByte()
@@ -36,7 +75,7 @@ func decodeBencode(reader *bytes.Reader) (interface{}, error) {
 		}
 		return data, nil
 	case c == 'd':
-		data := make(map[string]any)
+		data := NewOrderedMap()
 		for {
 			cur, _ := reader.ReadByte()
 			if cur == 'e' {
@@ -46,7 +85,7 @@ func decodeBencode(reader *bytes.Reader) (interface{}, error) {
 			key, _ := decodeBencode(reader)
 			value, _ := decodeBencode(reader)
 			if keyStr, ok := key.(string); ok {
-				data[keyStr] = value
+				data.Set(keyStr, value)
 				// Type assertion succeeded, and strValue is now of type string
 			} else {
 				return "", fmt.Errorf("not a valid key %v", key)
@@ -90,8 +129,12 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-
-		jsonOutput, _ := json.Marshal(decoded)
+    var jsonOutput []byte
+		if mapData, ok := decoded.(*OrderedMap); ok {
+			jsonOutput, _ = json.Marshal(mapData.GetMap())
+		} else {
+			jsonOutput, _ = json.Marshal(decoded)
+		}
 		fmt.Println(string(jsonOutput))
 	} else if command == "info" {
 		fileContent, err := os.ReadFile(os.Args[2])
@@ -104,14 +147,29 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		if data, ok := data.(map[string]any); ok {
-			fmt.Printf("Tracker URL: %s\n", data["announce"])
-			if info, ok := data["info"].(map[string]any); ok {
-        fmt.Printf("Length: %d\n", info["length"])
-        /* for k := range info { */
-        /*   fmt.Printf("key %s\n", k) */
-        /* } */
+		if data, ok := data.(*OrderedMap); ok {
+			announce, _ := data.Get("announce")
+			info, _ := data.Get("info")
+			fmt.Printf("Tracker URL: %s\n", announce)
+			if info, ok := info.(*OrderedMap); ok {
+				length, _ := info.Get("length")
+				fmt.Printf("Length: %d\n", length)
+				/* for k := range info { */
+				/*   fmt.Printf("key %s\n", k) */
+				/* } */
 			}
+			infoBytes, err := bencode(info)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			h := sha1.New()
+			h.Write(infoBytes)
+			bs := h.Sum(nil)
+			fmt.Printf("Info Hash: %x\n", bs)
+
+		} else {
+			fmt.Println("not ok")
 		}
 
 	} else {
